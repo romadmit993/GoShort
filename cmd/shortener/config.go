f13282136403a878bd5a95f0b3b0a3f6d8e97df9
romadmit993 <romadmit993@mail.ru>
2025-03-09 22:55:28 +0300

    iter5

--- cmd/shortener/config.go
+++ cmd/shortener/config.go
@@ -8,30 +8,42 @@ import (
 	"github.com/caarlos0/env/v6"
 )
 
+// Config хранит конфигурацию приложения.
 var Config struct {
 	localServer string
 	baseAddress string
 }
 
+// EnviromentVariables представляет переменные окружения.
 type EnviromentVariables struct {
-	serverAddress string `env:"SERVER_ADDRESS"`
-	baseUrl       string `env:"BASE_URL"`
+	ServerAddress string `env:"SERVER_ADDRESS"`
+	BaseURL       string `env:"BASE_URL"`
 }
 
 var cfg EnviromentVariables
 
+// ParseFlags загружает конфигурацию из переменных окружения и флагов.
 func ParseFlags() {
-
+	// Загружаем переменные окружения.
 	if err := env.Parse(&cfg); err != nil {
 		log.Fatalf("Ошибка при парсинге переменных окружения: %s", err)
 	}
 
-	flag.StringVar(&Config.localServer, "a", "localhost:8080", "start server")
-	flag.StringVar(&Config.baseAddress, "b", "http://localhost:8080/", "shorter URL")
+	// Устанавливаем значения по умолчанию для флагов.
+	flag.StringVar(&Config.localServer, "a", "localhost:8080", "адрес запуска HTTP-сервера")
+	flag.StringVar(&Config.baseAddress, "b", "http://localhost:8080/", "базовый адрес сокращённого URL")
 	flag.Parse()
 
+	// Приоритет: переменные окружения > флаги.
+	if cfg.ServerAddress != "" {
+		Config.localServer = cfg.ServerAddress
+	}
+	if cfg.BaseURL != "" {
+		Config.baseAddress = cfg.BaseURL
+	}
+
+	// Убедимся, что BaseAddress заканчивается на "/".
 	if !strings.HasSuffix(Config.baseAddress, "/") {
 		Config.baseAddress += "/"
 	}
-
 }
