60073232e4ff1b356af4599499941fb6022ed932
romadmit993 <romadmit993@mail.ru>
2025-03-24 21:48:46 +0300

    iter9

--- cmd/shortener/config.go
+++ cmd/shortener/config.go
@@ -20,25 +20,20 @@ var Config struct {
 type EnviromentVariables struct {
 	ServerAddress string `env:"SERVER_ADDRESS"`
 	BaseURL       string `env:"BASE_URL"`
-	FileStorage   string `env:"FILE_STORAGE_PATH"` // новое поле
+	FileStorage   string `env:"FILE_STORAGE_PATH"`
 }
 
 var cfg EnviromentVariables
 
-// ParseFlags загружает конфигурацию из переменных окружения и флагов.
 func ParseFlags() {
-	// Загружаем переменные окружения.
 	if err := env.Parse(&cfg); err != nil {
 		log.Printf("Ошибка при парсинге переменных окружения: %s", err)
 	}
-
-	// Устанавливаем значения по умолчанию для флагов.
 	flag.StringVar(&Config.localServer, "a", "localhost:8080", "адрес запуска HTTP-сервера")
 	flag.StringVar(&Config.baseAddress, "b", "http://localhost:8080/", "базовый адрес сокращённого URL")
-	flag.StringVar(&Config.fileStorage, "f", "/tmp/short-url-db.json", "путь к файлу для хранения данных") // исправлено значение по умолчанию
+	flag.StringVar(&Config.fileStorage, "f", "/tmp/short-url-db.json", "путь к файлу для хранения данных")
 	flag.Parse()
 
-	// Приоритет: переменные окружения > флаги.
 	if cfg.ServerAddress != "" {
 		Config.localServer = cfg.ServerAddress
 	}
@@ -48,7 +43,7 @@ func ParseFlags() {
 	if cfg.FileStorage != "" {
 		Config.fileStorage = cfg.FileStorage
 	}
-	// Убедимся, что BaseAddress заканчивается на "/".
+
 	if !strings.HasSuffix(Config.baseAddress, "/") {
 		Config.baseAddress += "/"
 	}
